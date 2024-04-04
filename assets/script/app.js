c5c2a8397c1f1ee21f2377681bef3e33db8cae63
Lubunga <innocentsadi6@gmail.com>
2024-04-03 23:34:55 -0500

    "final

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,27 +1,29 @@
 'use strict';
 
 const wordOutput = document.querySelector('h2');
-const h3test = document.querySelector('calculac');
 const input = document.querySelector('.input');
-// animation
+
 const border = document.getElementById('border');
 const counterText = document.getElementById('counter');
 const button = document.getElementById('start-count');
+
 const game = document.querySelector('.game');
 const finished = document.querySelector('.finished');
+const hits = document.querySelector('.hits');
 const start = document.querySelector('.start');
+const restart = document.querySelector('.restart');
+const score = document.querySelector('.score');
 
 
 const gameSound = new Audio('./assets/audio/game-music.mp3')
 gameSound.type = 'audio/mp3'
 gameSound.volume = 0.5;
 
-
 const correctSound = new Audio('./assets/audio/correct.mp3')
 correctSound.type = 'audio/mp3'
 
-const words = ['dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building', 
-'population', 'weather', 'bottle', 'history', 'dream', 'character', 'money', 
+const words = ['dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building', 'population',
+'weather', 'bottle', 'history', 'dream', 'character', 'money', 'absolute',
 'discipline', 'machine', 'accurate', 'connection', 'rainbow', 'bicycle',
 'eclipse', 'calculator', 'trouble', 'watermelon', 'developer', 'philosophy',
 'database', 'periodic', 'capitalism', 'abominable', 'component', 'future',
@@ -33,9 +35,10 @@ const words = ['dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building',
 'superman', 'library', 'unboxing', 'bookstore', 'language', 'homework',
 'fantastic', 'economy', 'interview', 'awesome', 'challenge', 'science', 'mystery',
 'famous', 'league', 'memory', 'leather', 'planet', 'software', 'update', 'yellow',
-'keyboard', 'window', 'absolute'];
+'keyboard', 'window'];
 
 let currentWordIndex = 0;
+let completedWords = 0;
 
 function shuffleArray(words) {
   for (let i = words.length - 1; i > 0; i--) {
@@ -47,41 +50,43 @@ function shuffleArray(words) {
 const shuffledWords = shuffleArray([...words]);
 
 function displayNextWord() {
-  wordOutput.textContent = shuffledWords[currentWordIndex];
+  wordOutput.textContent = shuffledWords[currentWordIndex].toUpperCase();
 }
 
-// Event listener for input field
+
+// INPUT - SHUFFLE - HITS
 input.addEventListener('input', function() {
   const inputValue = this.value.trim().toLowerCase();
   const correctValue = shuffledWords[currentWordIndex];
+
   if (inputValue === correctValue) {
     this.value = ''; // Clear the input field
     currentWordIndex++;
+    completedWords++
+    hits.innerText = `Hits: ${completedWords}`
+
     if (currentWordIndex < shuffledWords.length) {
       correctSound.play()
       displayNextWord();
     } else {
-      // All words entered correctly
-      finished.style.display = 'inline-block';
       game.classList.add("done")
     }
   }
 });
-// COUNTER ANIMATION
+
+// TIMER/ANIMATION
 let counter = 99;
 const duration = 99000;
 const increment = duration / counter;
 
-
 function startCountdown(timestamp) {
   let startTime = timestamp;
   let elapsedTime = 0;
-  console.log(counter)
 
   function animate(timestamp) {
     elapsedTime = timestamp - startTime;
 
-    if (elapsedTime < duration) {
+    if (elapsedTime < duration) {                 // counter animation
       const progress = elapsedTime / duration;
       const dashArray = (1 - progress) * 579.73;
       border.style.strokeDasharray = `${dashArray} 579.73`;
@@ -90,7 +95,7 @@ function startCountdown(timestamp) {
         border.classList.add("visible")
       }
       requestAnimationFrame(animate);
-    } else { // Reset counter
+    } else {                                      // Reset counter
       border.style.strokeDasharray = '0 579.73';
       counterText.textContent = '0';
       counter = 99; 
@@ -101,7 +106,7 @@ function startCountdown(timestamp) {
   }
   requestAnimationFrame(animate);
 }
-
+// PLAY
 button.addEventListener('click', function() {
   start.classList.add("visible")
   game.classList.add("visible")
@@ -110,3 +115,27 @@ button.addEventListener('click', function() {
   input.focus();
   gameSound.play();
 });
+// RESET
+function restartGame() {
+  input.value = ''
+
+  requestAnimationFrame(startCountdown);
+  border.style.strokeDasharray = '579.73';
+  counterText.textContent = 99;
+  counter = 99; 
+  
+  finished.classList.remove("visible");
+  game.classList.add("visible");
+  
+  gameSound.currentTime = 0;
+  gameSound.play();
+  
+  hits.innerText = 'Hits: 0';
+  completedWords = 0
+
+  currentWordIndex = 0;
+  displayNextWord();
+
+  input.focus();
+}
+restart.addEventListener('click', restartGame)
